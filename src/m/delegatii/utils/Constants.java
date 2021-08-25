24fd2faa108a79be41b0a1d34c447c4888dd9b8d
florin-b <gflorinb@yahoo.com>
2021-08-25 16:44:33 +0300

    Update Constants.java

--- src/m/delegatii/utils/Constants.java
+++ src/m/delegatii/utils/Constants.java
@@ -2,6 +2,6 @@ package m.delegatii.utils;
 
 public class Constants {
 
-	public static final String GOOGLE_MAPS_API_KEY = "AIzaSyCO29NKFuNalBKo4tXhsvHBWYGYT6aN7ZY";
+	public static final String GOOGLE_MAPS_API_KEY = "";
 
 }
